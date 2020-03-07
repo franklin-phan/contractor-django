bc0c422585e9ec2b6466fa90ec7d7f27d5c3b618
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2020-03-06 23:13:12 -0800

    heroku push

--- pantry/settings.py
+++ pantry/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = 'staticfiles'
-
+#hello
 # Where to redirect during authentication
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
